b3003d5db6cef75184a6a51cab2f78ce751dab8c
Toltuc <pikimi305@gmail.com>
2026-04-16 19:41:48 +0800

    Add classwork and SSMS project files

---
+++ SQL/SQLQuery1-INSERT Directions.sql
@@ -0,0 +1,6 @@
+﻿USE P_421_DDL;
+GO
+
+INSERT Directions(direction_id, direction_name)
+VALUES  (1, N'Разработка программного обеспечения'),
+        (2, N'Компьютерная графика и дизайн');
---
+++ SQL/SQLQuery_INSERT_Data.sql
@@ -0,0 +1,5 @@
+﻿USE P_421_DDL;
+
+INSERT Directions(direction_id, direction_name)
+VALUES  (1, N'Разработка программного обеспечения'),
+        (2, N'Комп')
